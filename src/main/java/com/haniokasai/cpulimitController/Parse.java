9677c5e30cdd3c5b3ce0f84b2ddafc7e9632b68d
haniokasai <htek@haniokasai.com>
2018-11-08 00:21:20 +0900

    ついに実装完了

--- src/main/java/com/haniokasai/cpulimitController/Parse.java
+++ src/main/java/com/haniokasai/cpulimitController/Parse.java
@@ -7,42 +7,61 @@ import java.util.List;
 
 
 public class Parse {
+
+    /**
+     * @return Block thread until result display.
+     */
     public static boolean iscpulimitEnable(){
-        CpulimitThread cp = new CpulimitThread(new String[]{"cpulimit","-h"});
-        cp.start();
-        try {
-            cp.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        final ArrayList <String> commandoutput = cp.outputlines;
-        if(commandoutput == null)return false;
+        int iii = 0;
+        while (iii!=2) {
+            iii++;
+
+            CpulimitThread cp = new CpulimitThread(new String[]{"cpulimit", "-h"});
+            cp.start();
+            try {
+                cp.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            final ArrayList<String> commandoutput = cp.outputlines;
+            if (commandoutput == null && Main.debug) System.out.println("i");
+            if (commandoutput != null) {
         /*
         CPUlimit version 2.4
         Usage: cpulimit TARGET [OPTIONS...] [-- PROGRAM]
         TARGET must be exactly one of these:
         */
-        for (String line : commandoutput) {
-            if (line.contains("CPUlimit")&&line.contains("version")) {
-                return true;
+                for (String line : commandoutput) {
+                    if (line.contains("CPUlimit") && line.contains("version")) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
     }
 
+    /**
+     * @return Block thread until result display.
+     */
     public static String getcpulimitVersion(){
-        CpulimitThread cp = new CpulimitThread(new String[]{"cpulimit","-h"});
-        cp.start();
-        try {
-            cp.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        final ArrayList <String> commandoutput = cp.outputlines;
-        if(commandoutput == null)return null;
-        for (String line : commandoutput) {
-            if (line.contains("CPUlimit")&&line.contains("version")) {
-                return line.replaceAll("CPUlimit","").replaceAll("version","").trim();
+        int iii = 0;
+        while (iii!=2) {
+            iii++;
+            CpulimitThread cp = new CpulimitThread(new String[]{"cpulimit", "-h"});
+            cp.start();
+            try {
+                cp.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            final ArrayList<String> commandoutput = cp.outputlines;
+            if (commandoutput != null) {
+                for (String line : commandoutput) {
+                    if (line.contains("CPUlimit") && line.contains("version")) {
+                        return line.replaceAll("CPUlimit", "").replaceAll("version", "").trim();
+                    }
+                }
             }
         }
         return null;
